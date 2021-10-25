e83d295ae57405fb9c3525ce6056a8a1eb206b0d
Sai Nyi <magsainyinyitun.tumdy@gmail.com>
2021-10-25 22:02:02 +0630

    First Averagine Point (random performance)

---
+++ coding/tools/Test1/Algorithm.py
@@ -0,0 +1,114 @@
+import numpy as np;
+from constructW import ConstructW;
+# from calculate_eigen import CalculateEigen;
+from sklearn.preprocessing import Normalizer;
+from update import Update;
+import matrix_utils as mu;
+class Algorithm():
+    
+    def __init__(self):
+        self.cw = ConstructW();
+        #self.cal_eig = CalculateEigen();
+        self.transformer = Normalizer();
+        self.upd = Update();
+        
+    def implement(self,VL,label,alpha,beta,gamma,lambdas):
+        
+        ZL,WL,HL = self.initialize(VL);  
+        NVL = self.normilize(VL);
+        # c = len(np.unique(label));
+        
+        LL = []; # for list of lapaclian matrix
+        eigValue = [];
+        eigVector = [];
+        FL = [];
+        
+        for j in range(len(VL)):
+            rel_error = 10;
+            normX = mu.norm_fro(NVL[j]);
+            while(rel_error > 0.1):
+                # print('Size of H',HL[j].shape);
+                # print('Size of W',WL[j].shape);
+                # print('Size of X',NVL[j].shape);
+                # print('Size of Z',ZL[j].shape);
+                
+                WL[j] = WL[j] * self.upd.update_w(NVL[j], HL[j], WL[j]);
+                HL[j] = HL[j] * self.upd.update_h(HL,HL[j], WL[j], NVL[j], ZL[j], j, alpha);
+                ZL[j] = ZL[j] * self.upd.update_z(ZL[j], WL[j], gamma);
+                
+                # print('Norm X :',normX);
+                # rel_error = mu.norm_fro_err(A, W, H, norm_A) / norm_A
+                rel_error = mu.norm_fro_err(NVL[j], WL[j], HL[j], normX) / normX;
+                print("Error is:",rel_error);
+                
+            LL.append(self.calculate_laplacian(ZL[j]));
+            value,vector = np.linalg.eig(LL[j]);
+            eigValue.append(value);
+            eigVector.append(vector);
+            
+            # x = zip(x, range(len(x)))
+            # x = sorted(x, key=lambda x:x[0])
+            eigValue[j] = zip(eigValue[j],range(len(eigValue[j])));
+            eigValue[j] = sorted(eigValue[j],key=lambda x:x[0]);
+            
+            # H = np.vstack([V[:,i] for (v, i) in x[:500]]).T
+            tempF = np.vstack([eigVector[j][:,i] for (v,i) in eigValue[j][:20]]).T;
+            FL.append(tempF.copy());
+            
+            
+        return WL,HL,ZL,LL,eigValue,eigVector,FL;
+                
+    def calculate_laplacian(self,X):
+        # laplacianMatrix = np.diag(degreeMatrix) - adjacentMatrix
+        # sqrtDegreeMatrix = np.diag(1.0 / (degreeMatrix ** (0.5)))
+        # return np.dot(np.dot(sqrtDegreeMatrix, laplacianMatrix), sqrtDegreeMatrix)
+        degreeM = np.sum(X,axis=1);
+        laplacianM = np.diag(degreeM) - X;
+        sqrtDegreeM = np.diag(1.0/(degreeM**(0.5)));
+        return np.dot(np.dot(sqrtDegreeM,laplacianM),sqrtDegreeM);
+        
+    
+    def calculate_eigen_properties(self,L):
+        return np.linalg.eig(L);
+        
+
+    
+    def initialize(self,V,k=20):
+        ZL = [];
+        WL = [];
+        HL = [];
+        for i in range(len(V)):
+            Ztemp = self.cw.SimilarityMatrix(V[i]);
+            Wtemp = np.random.rand(V[i].shape[0],k);
+            Htemp = np.random.rand(k,V[i].shape[1]);
+            
+            ZL.append(Ztemp.copy());
+            WL.append(Wtemp.copy());
+            HL.append(Htemp.copy());
+            
+        return ZL,WL,HL;
+
+    
+    def normilize(self,V):
+        NV = [];
+        for i in range(len(V)):
+            Vtemp = self.transformer.transform(V[i]);
+            NV.append(Vtemp.copy());
+        return NV;
+  
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ coding/tools/Test1/update.py
@@ -0,0 +1,96 @@
+import numpy as np;
+
+# X => (m x n )
+# W => (m x k)
+# H => (k x n)
+# Z => (n x n)
+
+# from calculate_eigen import CalculateEigen;
+
+class Update():
+    
+    # def __init__(self):
+    #     self.cal_eig = CalculateEigen();
+        
+    
+    def update_h(self,HL,H,W,X,Z,view_no,alpha):
+        # for middle loop
+        # mid_H=0;
+        # for i in range(len(HL)):
+        #     if(i == view_no):
+        #         continue;
+        #     mid_H = mid_H + HL[i]; # (kxn)
+        # ##
+        # mid_H = alpha*mid_H;
+        # print('Shape of midH sum:',mid_H.shape);
+        
+        upper = 2*(np.dot(W.T,X));   #   2*(W.T)(X) + 4*HF*Z # (kxm)(mxn) + (kxn)(nxn) => (kxn) 
+        lower = 2*( np.dot( np.dot(W.T,W) ,H) );
+        return upper/lower; # (k,n) matrix
+        
+    
+    def update_w(self,X,H,W):
+        # 2 *X * (H.T);         # (m,n)(n,k)
+        up = 2*(np.dot(X,H.T));
+        # W * H * (H.T)    # (m,k)(k,n)(n,k)
+        down = np.dot(np.dot(W,H),H.T);
+        return up/down; # (m,k) matrix
+    
+    def update_z(self,Z,W,gamma):
+        upper = np.dot(W,W.T);
+        lower = np.dot(np.dot(Z,W),W.T) + gamma/2;
+        return upper/lower; # (n , n) matrix
+        
+    # def update_s(self,w0,lambdas,beta,Z,F): 
+    #     # Z = [Z1,Z2,...]; list of all Z in multi-view 
+    #     n = Z.shape[0];
+        
+        
+    #     # Pi = np.zeros((n,n));
+    #     S = np.zeros((n,n));
+        
+    #     Zv = self._sum_of_Z(Z,w0); # vo -=> vector (w01,w02,w03,....)
+        
+    #     for i in range(n):
+    #         # Pi = self._solve_p(Pi,F,n,i);
+    #         Pi = self._solve_p( F, n, i);
+    #         S[:,i] = ( Zv[:,i] - beta*(Pi.T)/4*lambdas ) / 4*np.sum(w0);
+        
+    #     return S;
+            
+        
+    
+    # def update_wo(self,Z,S):
+    #     # print(np.linalg.norm(a, 'fro'))
+    #     return 1/2*(np.linalg.norm((Z-S),'fro'));
+        
+    # def solve_f(self,X,c=None,isMax=1,isSym=1):
+    #     F,_,_= self.cal_eig.calculate(X,c,isMax,isSym);
+    #     return F;
+        
+        
+    # def _solve_p(self,F,n,i):
+    #     for j in range(n):
+    #         diff = F[i,:] - F[j,:];
+    #         Pi[j] = np.square( np.linalg.norm(diff) );
+    #     return P;
+
+    # def _sum_of_Z(self,Z,w0):
+    #     Zv = np.zeros_like(Z[0]); # n x n 
+    #     for i in range(len(Z)):
+    #         Zv = Zv + w0[i]*Z[i]; # w0[0] * Z[0] + w0[1] * Z[1] + .... +
+    #     return Zv;
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
